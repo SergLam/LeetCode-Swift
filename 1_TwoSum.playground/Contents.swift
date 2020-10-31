99a2cbad323f520adcbea4594d80f049af0c4e61
SergLam <lamcevs0@gmail.com>
2020-10-31 14:04:09 +0200

    Two sum problem solution

--- 1_TwoSum.playground/Contents.swift
+++ 1_TwoSum.playground/Contents.swift
@@ -4,7 +4,7 @@ import PlaygroundSupport
 class Solution {
     
     // MARK: - Brute force solution
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    func twoSumForce(_ nums: [Int], _ target: Int) -> [Int] {
         
         for (index1, num1) in nums.enumerated() {
             for i in stride(from: index1 + 1,to: nums.count, by: 1) {
@@ -17,8 +17,45 @@ class Solution {
         return []
     }
     
+    // MARK: - Two-pass Hash Table
+    func twoSumTwoPassHashTable(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var hashMap: [Int: Int] = [:]
+        for (index, num) in nums.enumerated() {
+            hashMap[num] = index
+        }
+        
+        for (index, num) in nums.enumerated() {
+            let complement = target - num
+            if let index2 = hashMap[complement], (hashMap.keys.contains(complement) && index2 != index) {
+                return [index, index2]
+            }
+        }
+        return []
+    }
+    
+    // MARK: - One-pass Hash Table
+    func oneSumTwoPassHashTable(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var hashMap: [Int: Int] = [:]
+        for (index, num) in nums.enumerated() {
+            let complement = target - num
+            if hashMap.keys.contains(complement), let index2 = hashMap[complement] {
+                return [index, index2]
+            }
+            hashMap[num] = index
+        }
+        
+        return []
+    }
+    
+    
 }
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-Solution().twoSum([3,2,4], 6)
+Solution().twoSumForce([3,2,4], 6)
+
+Solution().twoSumTwoPassHashTable([3,2,4], 6)
+
+Solution().oneSumTwoPassHashTable([3,2,4], 6)
