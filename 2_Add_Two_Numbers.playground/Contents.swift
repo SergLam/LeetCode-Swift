233a4167407305be7d16061b149b5a1aa62c778c
SergLam <lamcevs0@gmail.com>
2020-11-01 20:01:13 +0200

    Two linked list sum

--- 2_Add_Two_Numbers.playground/Contents.swift
+++ 2_Add_Two_Numbers.playground/Contents.swift
@@ -28,8 +28,45 @@ public class ListNode {
 class Solution {
     
   func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-      return nil
+    
+    let head: ListNode? = ListNode(0)
+    var p: ListNode? = l1
+    var q: ListNode? = l2
+    var curr: ListNode? = head
+    
+    var carry: Int = 0
+    
+    while (p != nil || q != nil) {
+        
+        let x: Int = p?.val ?? 0
+        let y: Int = q?.val ?? 0
+        let sum: Int = carry + x + y
+        print(sum)
+        
+        carry = sum / 10
+        
+        curr?.next = ListNode(sum % 10)
+        curr = curr?.next
+        print(curr?.val, curr?.next)
+        
+        p = p?.next
+        q = q?.next
+        print(head?.val, head?.next, head?.next?.val, head?.next?.next?.val, head?.next?.next?.next?.val)
+    }
+    if (carry > 0) {
+        curr?.next = ListNode(carry)
+    }
+    return head?.next
   }
     
 }
 
+// [2,4,3]
+// [5,6,4]
+
+// [7,0,8]
+
+let l1: ListNode = ListNode(2, ListNode(4, ListNode(3)))
+let l2: ListNode = ListNode(5, ListNode(6, ListNode(4)))
+
+let result = Solution().addTwoNumbers(l1, l2)
